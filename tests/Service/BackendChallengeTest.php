d2909a9b037e2a880a2f9bd4aaeed76dab47d6d9
Tony Briceño <tonybp18@gmail.com>
2017-06-05 00:05:38 -0400

    Test case: test_construct_not_null

--- tests/Service/BackendChallengeTest.php
+++ tests/Service/BackendChallengeTest.php
@@ -2,6 +2,8 @@
 namespace Linio\Recruiting\Service;
 
 use Linio\Recruiting\Implementations\DefaultOutputFormat;
+use Linio\Recruiting\Implementations\JsonOutputFormat;
+use Linio\Recruiting\Implementations\XmlOutputFormat;
 
 /**
  * @package Linio\Recruiting\Service
@@ -9,5 +11,14 @@ use Linio\Recruiting\Implementations\DefaultOutputFormat;
  */
 class BackendChallengeTest extends \PHPUnit_Framework_TestCase
 {
+	/**
+	 * @covers ::__construct
+	 */
+	public function test_construct_not_null()
+    {
+    	$backendChallenge = new BackendChallenge(new DefaultOutputFormat);
+    	
+    	$this->assertNotNull($backendChallenge);
+    }
 
 }
