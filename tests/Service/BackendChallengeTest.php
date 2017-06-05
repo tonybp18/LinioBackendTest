6785d209d0b2f32b285cb49cd13ba09b0bac49bd
Tony Briceño <tonybp18@gmail.com>
2017-06-05 00:07:32 -0400

    Test case: test_printChallenge_prints_0_elements_with_cero_or_negative_parameter

--- tests/Service/BackendChallengeTest.php
+++ tests/Service/BackendChallengeTest.php
@@ -47,4 +47,38 @@ class BackendChallengeTest extends \PHPUnit_Framework_TestCase
         // Checks default number of elements
         $this->assertCount(100, $outputStringAsArray);
     }
+
+    /**
+     * @runInSeparateProcess
+	 * @covers ::printChallenge
+	 */
+	public function test_printChallenge_prints_0_elements_with_cero_or_negative_parameter()
+    {
+    	$backendChallenge = new BackendChallenge(new DefaultOutputFormat);
+    	
+    	// Call with parameter equals 0
+    	$backendChallenge->printChallenge(0);
+
+    	$outputString = ob_get_contents();
+        $outputString = rtrim($outputString);
+
+        $outputStringAsArray = explode(' ', $outputString);
+        
+        // Checks if has one element and is an empty string
+        $this->assertCount(1, $outputStringAsArray);
+        $this->assertEquals("", $outputStringAsArray[0]);
+
+
+    	// Call with a negative number
+        $backendChallenge->printChallenge(-20);
+
+    	$outputString = ob_get_contents();
+        $outputString = rtrim($outputString);
+
+        $outputStringAsArray = explode(' ', $outputString);
+        
+        // Checks if has one element and is an empty string
+        $this->assertCount(1, $outputStringAsArray);
+        $this->assertEquals("", $outputStringAsArray[0]);
+    }
 }
